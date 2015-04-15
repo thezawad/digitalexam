cd8ed6e827818c3840736283a70b61e2582210e0
Zawad Bin Hafiz <thezawad@users.noreply.github.com>
2015-04-15 11:47:45 +0600

    Create header_exam.php

---
+++ header_exam.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(0);
+echo "<head>
+<title>Digital Exam</title>
+</head>";
+echo "<body background=\"images/bubble.png\">
+<center><font color=\"blue\"><h1>Digital Exam</h1></font></center></body>";
+//echo "<b><a href=\"index.php\">HOME</a>&nbsp;&nbsp;||&nbsp;&nbsp;<a href=\"manex.php\">Manage Exam</a>&nbsp;&nbsp;||&nbsp;&nbsp;<a href=\"addex.php\">Create Exam</a>&nbsp;&nbsp;||&nbsp;&nbsp;<a href=\"question.php\">Add Question</a>&nbsp;&nbsp;||&nbsp;&nbsp;<a href=\"ed_exam.php\">View Exam</a>&nbsp;&nbsp;||&nbsp;&nbsp;<a href=\"exchoice.php\">Exam Final View</a></b>";
+
+?>
