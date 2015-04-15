f4929412ad2766712bf6ccd0cce73c2853361a6a
Zawad Bin Hafiz <thezawad@users.noreply.github.com>
2015-04-15 11:46:53 +0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,3 @@
+<?php
+echo "<center><font color=\"red\"><b>Programmed by: <a href=\"about.php\">DreamTeam ALDC</a></b></font>&nbsp;&nbsp;&nbsp;||&nbsp;&nbsp;&nbsp;<a href=\"index.php\">Log Out</a></center>";
+?>
