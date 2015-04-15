4454e5375862dc957f91c71131319d1594d5f4f6
Zawad Bin Hafiz <thezawad@users.noreply.github.com>
2015-04-15 11:47:25 +0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(0);
+echo "<head>
+<title>Digital Exam System  || DreamTeam ALDC</title>
+</head>";
+echo "<body background=\"images/bubble.png\">
+<center><font color=\"blue\"><h1>Digital Exam System</h1></font></center></body>";
+echo "<center><b><a href=\"manex.php\">Manage Exam</a>&nbsp;&nbsp;||&nbsp;&nbsp;<a href=\"addex.php\">Create Exam</a>&nbsp;&nbsp;||&nbsp;&nbsp;<a href=\"question.php\">Add Question</a>&nbsp;&nbsp;||&nbsp;&nbsp;<a href=\"ed_exam.php\">View Exam</a>&nbsp;&nbsp;||&nbsp;&nbsp;<a href=\"exchoice.php\">Exam Final View</a>&nbsp;&nbsp;||&nbsp;&nbsp;<a href=\"detex.php\">Check Result</a></b></center>";
+
+?>
