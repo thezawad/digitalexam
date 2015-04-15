5c2a5cc70f42a083fd18e3734740eb736f4238f0
Zawad Bin Hafiz <thezawad@users.noreply.github.com>
2015-04-15 11:42:08 +0600

    Create about.php

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php
+include('header.php');
+?>
+<center><br/><br/>
+<font color="blue" size="6">About The Software Developer</font><br/><br/>
+<p><b> Hello there!<br/>We are a group of passionate software developer from the renowned Amrita Lal Dey College<br/>
+This is me ->  <a href="mailto:zleo99@gmail.com">Zawad Bin Hafiz</a> The programmer and Idea Maker of this software<br/>
+And my friend -> <a href="mailto:sadatulhassans@gmail.com">Sadat Ul Hassan</a> The Encourager and Tester of this software<br/>
+And my another friend -> <font color="blue"><u>Afran Mahmud</u></font> The Encouraged and Supporter of this software<br/>
+<br/>
+And we together built this software with love keeping our classroom in mind so that something good comes out of our mind. And there you see the result!<br/>
+<font color="blue" size="4">Hope you like it. Give us a big thumbs up then (Y)</font></b></p>
+</center>
+<?php include('footer.php'); ?>
